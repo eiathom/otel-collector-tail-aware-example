3853fca1f682be5d128a381a964552a95962044c
eiathom <eiathom@protonmail.ch>
2024-02-10 17:39:09 +0000

    [TailAware] Adds docs to crete parameters

--- scripts/convert_file_content_to_string.bash
+++ scripts/convert_file_content_to_string.bash
@@ -22,4 +22,4 @@ if [ ! -n "${file_full_path}" ]; then
 fi
 
 # convert content to string
-echo "\"$(cat ${file_full_path})\""
+echo "$(cat ${file_full_path})"
--- scripts/create_update_system_variable.bash
+++ scripts/create_update_system_variable.bash
@@ -3,6 +3,7 @@ set -o pipefail
 
 PARAMETER_KEY_NAME=${PARAMETER_KEY_NAME}
 PARAMETER_VALUE=${PARAMETER_VALUE}
+SECURE_STRING=${SECURE_STRING}
 
 # check variable is set
 if [ -z "${PARAMETER_KEY_NAME}" ]; then
@@ -23,12 +24,22 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# put the parameter
-${aws_command} ssm put-parameter --name "${PARAMETER_KEY_NAME}" --value "${PARAMETER_VALUE}" --type String --overwrite > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "ERROR: Unable to create/update variable '${PARAMETER_KEY_NAME}', exiting"
-  exit 1
+if [ -z "${SECURE_STRING}" ]; then
+  # put the parameter
+  ${aws_command} ssm put-parameter --name "${PARAMETER_KEY_NAME}" --value "${PARAMETER_VALUE}" --type String --overwrite > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to create/update variable '${PARAMETER_KEY_NAME}', exiting"
+    exit 1
+  fi
+else
+  # put the secure parameter
+  ${aws_command} ssm put-parameter --name "${PARAMETER_KEY_NAME}" --value "${PARAMETER_VALUE}" --type SecureString --overwrite > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to create/update variable '${PARAMETER_KEY_NAME}', exiting"
+    exit 1
+  fi
 fi
+
 
 # check the parameter exists
 ${aws_command} ssm get-parameter --name "${PARAMETER_KEY_NAME}" > /dev/null 2>&1
